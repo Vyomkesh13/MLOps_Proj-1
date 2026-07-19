6252f00c5ded303c89f4f7aae796de23e2599d27
Vyom <vyomkeshsharma9231@gmail.com>
2026-07-19 20:58:57 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name  = "src",
+    version="0.0.1",
+    author="Vyomkesh Sharma",
+    author_email="vyomkeshsharma9231@gmail.com",
+    packages=find_packages()
+)
